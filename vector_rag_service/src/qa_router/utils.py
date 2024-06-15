d6042e491b8d222260cfd0a07c5eb90c1909cebf
Mikhail Kuznecov <kuznecovmihail2019@gmail.com>
2024-06-15 19:51:34 +0300

    use_documents_db_thought tool

--- vector_rag_service/src/qa_router/utils.py
+++ vector_rag_service/src/qa_router/utils.py
@@ -1,5 +1,6 @@
 from typing import List, Any
 
+from qa_router.prompts import USER_PROMPT_QA, SYSTEM_PROMPT_THOUGH, USER_PROMPT_THOUGH, SYSTEM_PROMPT_QA
 from qa_router.schemas import Input
 from config import settings
 from database.queries import search_relevant_chunks
@@ -7,23 +8,15 @@ from database.queries import search_relevant_chunks
 from qa_router.schemas import Answer
 
 
-def create_qa_prompt(context: str, question: str) -> str:
-    return f'''
-Используй следующие фрагменты контекста, чтобы ответить на вопрос в конце. Если ты не знаешь ответа, просто скажи, что ты не знаешь, не пытайся придумать ответ.
-
-{context}
-
-Вопрос: {question}
-Полезный ответ:
-    '''
-
-
 async def generate_answer(input: Input):
-    embedding = await create_embedding(input.query)
-    result = await search_relevant_chunks(vault_id=input.vault_id, vector=embedding, top_k=2)
-    qa_prompt = create_qa_prompt(context=result.text, question=input.query)
-    answer = await create_completion(qa_prompt)
-    return Answer(content=answer, traceback=result.search_result)
+    if await use_documents_db_thought(messages_history=input.history, query=input.query):
+        embedding = await create_embedding(input.query)
+        result = await search_relevant_chunks(vault_id=input.vault_id, vector=embedding, top_k=4, score_threshold=0.2)
+        answer = await create_completion_with_context(query=input.query, context=result.text, messages_history=input.history)
+        return Answer(content=answer, traceback=result.search_result)
+
+    answer = await create_base_completion(input.history + [{"user": input.query}])
+    return Answer(content=answer, traceback=[])
 
 
 async def create_embedding(query: str) -> List[float]:
@@ -32,10 +25,32 @@ async def create_embedding(query: str) -> List[float]:
     return list(map(lambda x: x.embedding, response.data))[0]
 
 
-async def create_completion(prompt: str) -> str:
+async def create_completion_with_context(context: Any, query: str, messages_history: list) -> str:
+    messages = [{"system": SYSTEM_PROMPT_QA}] + [messages_history] + [{"user": USER_PROMPT_QA.format(query=query,
+                                                                                                     context=context)}]
+    answer = await create_base_completion(messages)
+    return answer
+
+
+async def use_documents_db_thought(messages_history: list, query: str) -> bool:
+    messages = [{"system": SYSTEM_PROMPT_THOUGH}] + messages_history + [{"user": USER_PROMPT_THOUGH.format(query=query)}]
+    answer = await create_base_completion(messages)
+
+    if "0" in answer:
+        return False
+    else:
+        return True
+
+
+async def create_base_completion(messages: list) -> str:
     settings.openai_client.base_url = settings.URL_TO_LLM
-    response = await settings.openai_client.completions.create(model="lightblue/suzume-llama-3-8B-multilingual",
-                                                               prompt=prompt,
-                                                               temperature=0.0,
-                                                               max_tokens=1024)
-    return response.choices[0].text.replace('<|eot_id|>', '')
+    response = await settings.openai_client.chat.completions.create(model="lightblue/suzume-llama-3-8B-multilingual",
+                                                                    messages=messages,
+                                                                    temperature=0.0,
+                                                                    max_tokens=1024)
+    answer = response.choices[0].text
+
+    if "<|eot_id|>" in answer:
+        answer.replace('<|eot_id|>', '')
+
+    return answer
