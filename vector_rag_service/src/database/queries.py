feb966c0003f55e71797bc465a765054f0d24dbe
Mikhail Kuznecov <kuznecovmihail2019@gmail.com>
2024-06-15 19:52:15 +0300

    score_threshold, document_name

--- vector_rag_service/src/database/queries.py
+++ vector_rag_service/src/database/queries.py
@@ -7,8 +7,14 @@ from qa_router.schemas import SearchResult
 RelevantChunksResult = NamedTuple('RelevantChunksResult', [('text', str), ('search_result', List[SearchResult])])
 
 
-async def search_relevant_chunks(vault_id: str, vector: list[float], top_k: int) -> RelevantChunksResult:
-    response = await qdrant_store.search(collection_name=vault_id, query_vector=vector, limit=top_k)
+async def search_relevant_chunks(vault_id: str, vector: list[float], top_k: int, score_threshold: float) -> RelevantChunksResult:
+    response = await qdrant_store.search(collection_name=vault_id,
+                                         query_vector=vector,
+                                         limit=top_k,
+                                         score_threshold=score_threshold)
+
     result_text = '\n\n'.join(list(map(lambda x: x.payload['page_content'], response)))
-    search_result = [SearchResult(document_id=x.payload['document_id'], information=x.payload['page_content']) for x in response]
+    search_result = [SearchResult(document_id=x.payload['document_id'],
+                                  information=x.payload['page_content'],
+                                  document_name=x.payload['document_name']) for x in response]
     return RelevantChunksResult(text=result_text, search_result=search_result)
